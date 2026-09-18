57c7e17635048942e743f25ddc3f3f0e8a6f80bd
jtp229 <jtp229@lehigh.edu>
2026-09-17 20:16:44 -0400

    binary search tree

--- p1/scheme/tree.scm
+++ p1/scheme/tree.scm
@@ -53,5 +53,94 @@
 ;; exported by this file should be the `make-bst` function.
 
 (define (make-bst)
-  #f ;; [CSE 262] Implement Me!
-)
+  (define (insert tree val)
+  ;;helper function for insert
+    (cond
+    ;;if tree is empty make a node with two empty subtrees
+      ((null? tree) (list val '() '()))
+      ;;if the value is less then the root, keep the root and right tree the same, add value to left tree
+      ((< val (car tree)) (list (car tree) (insert (cadr tree) val) (caddr tree)))
+      ;;if the value is greater then the root, insert it into the right tree, leave the left and root alone
+      ((> val (car tree)) (list (car tree) (cadr tree) (insert (caddr tree) val)))
+      ;;else we have the root
+      (else tree)))
+  
+  (define (insert-all tree lst)
+  ;;take a tree and list of values and insert them
+    (if (null? lst)
+    ;;if list of values is empty were done inserting, return the tree
+        tree
+        ;;insert 1st element into tree then recursively insert rest of list insto tree
+        (insert-all (insert tree (car lst)) (cdr lst))))
+  ;;display the tree
+  (define (display-tree tree)
+    (if (null? tree)
+    ;;if tree is empty print nothing
+        (display "()")
+        (begin
+        ;;recursively print 
+          (display "(")
+          (display (car tree))
+          ;;root value
+          (display " ")
+          (display-tree (cadr tree))
+          ;;left subtree
+          (display " ")
+          ;;right subtree
+          (display-tree (caddr tree))
+          (display ")"))))
+  ;;in order traverse LEFT NODE RIGHT
+  (define (inorder-traverse tree f)
+
+    (if (not (null? tree))
+    ;;if the tree isnt empty, recursively go through left subtree
+        (begin
+          (inorder-traverse (cadr tree) f)
+          ;;apply function to node value
+          (f (car tree))
+          ;;go through left subtree
+          (inorder-traverse (caddr tree) f))))
+  ;;traverse with function NODE LEFT RIGHT
+  (define (preorder-traverse tree f)
+    (if (not (null? tree))
+        (begin
+        ;;apply function to current node
+          (f (car tree))
+          ;;then left tree
+          (preorder-traverse (cadr tree) f)
+       
+          (preorder-traverse (caddr tree) f))))
+      
+  
+  (let ((root '()))
+  ;;hold tree data
+  ;;message and argument as parameters
+    (lambda (msg arg)
+      (cond
+      ;;message for inserting a single value
+        ((eq? msg 'ins)
+         (set! root (insert root arg))
+         ;;update root and return ok when were done
+         'ok)
+         ;;make root empty return ok
+        ((eq? msg 'clear)
+         (set! root '())
+         'ok)
+         ;;insert all values from list
+        ((eq? msg 'inslist)
+         (set! root (insert-all root arg))
+         'ok)
+         ;;print tree using display
+        ((eq? msg 'display)
+         (display-tree root)
+         'ok)
+         ;;traverse in order and apply arg to each node
+        ((eq? msg 'inorder)
+         (inorder-traverse root arg)
+         'ok)
+         ;;traverse in preorder and apply arg
+        ((eq? msg 'preorder)
+         (preorder-traverse root arg)
+         'ok)
+         ;;if nothing matches throw an error
+        (else 'error)))))
